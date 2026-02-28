2123df663c2bc9aa9bc4e86c79092947b4c3537d
kubarodziewicz <kuba.rodziewicz@cyfroweszkoly.pl>
2026-02-28 20:23:24 +0100

    Add JSON parser and adapter for productList

--- app/src/main/java/com/example/mobile_smart_pantry_project_iv/Views/MainActivity.kt
+++ app/src/main/java/com/example/mobile_smart_pantry_project_iv/Views/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mobile_smart_pantry_project_iv.Views
 
 import android.os.Bundle
 import android.util.Log
+import android.util.Log.*
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.ListAdapter
 import com.example.mobile_smart_pantry_project_iv.Models.Product
 import com.example.mobile_smart_pantry_project_iv.R
 import com.example.mobile_smart_pantry_project_iv.databinding.ActivityMainBinding
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
 
@@ -34,7 +36,25 @@ class MainActivity : AppCompatActivity() {
 
         listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, productList)
         binding.pantryListView.adapter = listAdapter
+
         loadProductsFromJsonFile()
+    }
+
+
+    fun saveProductsToJsonFile() {
+        try {
+            val json = Json {prettyPrint = true}
+            val jsonString = json.encodeToString(productList)
+
+            val file = File(filesDir, "pantry.json")
+            file.writeText(jsonString)
+
+            Toast.makeText(this, "Zapisano dane (${productList.size} obiektów) do pliku json.", Toast.LENGTH_SHORT).show()
+
+        } catch (e: Exception) {
+            Toast.makeText(this, "Wystapil blad przy zapisie danych", Toast.LENGTH_SHORT).show()
+            Log.i("blad", e.message.toString())
+        }
     }
 
      fun loadProductsFromJsonFile() {
@@ -51,12 +71,8 @@ class MainActivity : AppCompatActivity() {
 
              listAdapter.notifyDataSetChanged()
 
-             if (productList.isEmpty()) Log.d("cos", "pusta lista")
-             else {
-                 Log.d("cos", "pelna lista")
-             }
          } catch(e: Exception) {
-             Toast.makeText(this, "wystapil blad loadProductsFromJsonFile", Toast.LENGTH_SHORT).show()
+             Toast.makeText(this, "wystapil blad przy zaladowaniu danych", Toast.LENGTH_SHORT).show()
          }
          }
 
